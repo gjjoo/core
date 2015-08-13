9cb43916320f001cf69b10b2ee3552a9d9b6b463
gjjoo <joo88tg7108@naver.com>
2015-08-13 10:31:03 +0900

    인구통계 스크립트 추

--- js/core.js
+++ js/core.js
@@ -8,6 +8,7 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
 ga('create', 'UA-66202604-1', 'auto');
+ga('require', 'displayfeatures');
 ga('require', 'linkid', 'linkid.js');
 ga('send', 'pageview');
 /*
